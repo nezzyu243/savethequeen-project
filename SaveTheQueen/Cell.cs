4e807b7c3bc4b6e958889cf3d0d87ab1e6fcce8b
nezzyu243 <kirkdushi@gmail.com>
2026-06-21 20:54:04 +0200

    dodanie klas Cell i Map, szkic mapy

---
+++ SaveTheQueen/Cell.cs
@@ -0,0 +1,12 @@
+namespace SaveTheQueen;
+
+// uzupelnic o character/player/npc po dodaniu tych klas
+public class Cell
+{
+    public char Visuals;
+
+    public void Display()
+    {
+        Console.Write(Visuals);
+    }
+}
---
+++ SaveTheQueen/Map.cs
@@ -0,0 +1,51 @@
+namespace SaveTheQueen;
+
+public class Map
+{
+    private Cell[][] _cells = [];
+
+    public void LoadFromFile(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+        _cells = new Cell[lines.Length][];
+
+        for (var rowIndex = 0; rowIndex < lines.Length; rowIndex++)
+        {
+            string line = lines[rowIndex];
+            _cells[rowIndex] = new Cell[line.Length];
+
+            for (var columnIndex = 0; columnIndex < line.Length; columnIndex++)
+            {
+                _cells[rowIndex][columnIndex] = new Cell { Visuals = line[columnIndex] };
+            }
+        }
+    }
+
+    public void Display()
+    {
+        Console.SetCursorPosition(0, 0);
+        foreach (Cell[] row in _cells)
+        {
+            foreach (Cell cell in row)
+            {
+                cell.Display();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        return _cells[y][x];
+    }
+
+    public int GetHeight()
+    {
+        return _cells.Length;
+    }
+
+    public int GetRowWidth(int row)
+    {
+        return _cells[row].Length;
+    }
+}
