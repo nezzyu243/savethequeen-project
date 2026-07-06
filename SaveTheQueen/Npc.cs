0db57590c41ee954c1940f943ef94f8295e58ef6
Gabriela Piecula <gpiecula@st.swps.edu.pl>
2026-07-06 17:51:54 +0200

    Fix List in Npc Class

--- SaveTheQueen/Npc.cs
+++ SaveTheQueen/Npc.cs
@@ -2,35 +2,36 @@ namespace SaveTheQueen;
 
 public class Npc : Character
 {
-    private readonly List<Vector2> availableDirections = new List<Vector2>
-    {
-        new Vector2(-1, 0),
-        new Vector2(1, 0),
-        new Vector2(0, -1),
-        new Vector2(0, 1)
-    };
+private readonly List<Vector2> availableDirections = new()
+{
+    new Vector2(-1, 0),
+    new Vector2(1, 0),
+    new Vector2(0, -1),
+    new Vector2(0, 1)
+};
 
-    public bool IsHostile { get; }
+public bool IsHostile { get; }
 
-    public Npc(char avatar, Map map, bool isHostile, int maxHp = 10)
-        : base(avatar, GetRandomPosition(map), map, maxHp)
-    {
-        IsHostile = isHostile;
-    }
+public Npc(char avatar, Map map, bool isHostile, int maxHp = 10)
+    : base(avatar, GetRandomPosition(map), map, maxHp)
+{
+    IsHostile = isHostile;
+}
 
-    private static Vector2 GetRandomPosition(Map map)
-    {
-        List<Vector2> floors = map.GetFloorPositions();
-        return floors[Random.Shared.Next(floors.Count)];
-    }
+private static Vector2 GetRandomPosition(Map map)
+{
+    List<Vector2> floors = map.GetFloorPositions();
+    return floors[Random.Shared.Next(floors.Count)];
+}
 
-    public override bool TakeTurn(Map map, List<Character> others)
-    {
-        if (!IsAlive) return true;
+public override bool TakeTurn(Map map, List<Character> others)
+{
+    if (!IsAlive) return true;
 
-        int index = Random.Shared.Next(availableDirections.Count);
-        Vector2 direction = availableDirections[index];
-        Move(direction, map);
-        return true;
-    }
+    int index = Random.Shared.Next(availableDirections.Count);
+    Vector2 direction = availableDirections[index];
+    Move(direction, map);
+    return true;
+
+}
 }
